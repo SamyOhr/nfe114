40725e4f77e3cac105c97b60ada06a8c1c784550
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-02 10:11:37 +0100

    a

--- POI.php
+++ POI.php
@@ -2,7 +2,7 @@
 
 //C'est le POI de l'utilisateur
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
-echo "43.296482\t5.36978\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
+echo "43.296482\t5.36978\tMoi\tMa Position\tpngegg.png\t24,24\t0,-24\n";
 echo "48.842053\t2.355763\tMusée d'Histoire Naturelle de Paris\tAdresse du Musée\tOl_icon_red_example.png\t24,24\t0,-24\n";
 
 
