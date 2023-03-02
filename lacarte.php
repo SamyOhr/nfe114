c393fd39bda367227a12a6656b9880a274aa5cec
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-02 11:03:50 +0100

    push

--- lacarte.php
+++ lacarte.php
@@ -1,13 +1,11 @@
 <?php
-//Récupération des coordonnées GPS choisies par l'utilisateur
-if (isset($_POST['lat']) && isset($_POST['lon'])) {
-  $lat = $_POST['lat'];
-  $lon = $_POST['lon'];
-} else {
-  //Si les coordonnées n'ont pas encore été entrées, on initialise à un emplacement par défaut
-  $lat = 43.296482;
-  $lon = 5.36978;
-}
+  if(isset($_POST['lat']) && isset($_POST['lon'])) {
+    $lat = $_POST['lat'];
+    $lon = $_POST['lon'];
+  } else {
+    $lat = 43.296482;
+    $lon = 5.36978;
+  }
 ?>
 
 <html>
@@ -16,7 +14,7 @@ if (isset($_POST['lat']) && isset($_POST['lon'])) {
   </head>
   <body>
     <div id="mapdiv"></div>
-    <form method="post">
+    <form method="post" action="">
       Latitude : <input type="text" name="lat" id="lat" value="<?php echo $lat; ?>">
       Longitude: <input type="text" name="lon" id="lon" value="<?php echo $lon; ?>">
       <input type="submit" value="Afficher">
@@ -25,14 +23,13 @@ if (isset($_POST['lat']) && isset($_POST['lon'])) {
       var map = new OpenLayers.Map("mapdiv");
       map.addLayer(new OpenLayers.Layer.OSM());
 
-      //Créer un marqueur à l'emplacement choisi
-      var lonLat = new OpenLayers.LonLat(<?php echo $lon; ?>, <?php echo $lat; ?>).transform(
-        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-        map.getProjectionObject() // to Spherical Mercator Projection
-      );
-      var marker = new OpenLayers.Layer.Markers( "Markers" );
-      marker.addMarker(new OpenLayers.Marker(lonLat));
-      map.addLayer(marker);
+      var markers = new OpenLayers.Layer.Markers( "Markers" );
+      map.addLayer(markers);
+
+      function addMarker(lonLat) {
+        markers.clearMarkers();
+        markers.addMarker(new OpenLayers.Marker(lonLat));
+      }
 
       function updateMap() {
         var lat = document.getElementById("lat").value;
@@ -43,18 +40,17 @@ if (isset($_POST['lat']) && isset($_POST['lon'])) {
         );
         var zoom = 12;
         map.setCenter(lonLat, zoom);
-
-        //Supprimer le marqueur existant
-        map.removeLayer(marker);
-
-        //Créer un nouveau marqueur à l'emplacement choisi
-        marker = new OpenLayers.Layer.Markers( "Markers" );
-        marker.addMarker(new OpenLayers.Marker(lonLat));
-        map.addLayer(marker);
+        addMarker(lonLat);
       }
 
       //Set start centrepoint and zoom
-      map.setCenter(lonLat, 12);
+      var lonLat = new OpenLayers.LonLat(<?php echo $lon; ?>, <?php echo $lat; ?>).transform(
+        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+        map.getProjectionObject() // to Spherical Mercator Projection
+      );
+      var zoom = 12;
+      map.setCenter(lonLat, zoom);
+      addMarker(lonLat);
     </script>
   </body>
 </html>
