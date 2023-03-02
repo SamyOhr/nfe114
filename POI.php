b839c0e5bf682df8fdd0c20631c413b83674185b
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-02 10:54:27 +0100

    a

--- POI.php
+++ POI.php
@@ -1,34 +1,22 @@
 <?php
-
-// Connexion à la BDD
-$base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
-$base->exec("SET CHARACTER SET utf8");
-
-// Récupération des coordonnées de l'utilisateur
+// Vérifiez que les variables lat et lon ont été passées dans la requête GET
 if (isset($_GET['lat']) && isset($_GET['lon'])) {
+  // Récupérez les coordonnées saisies par l'utilisateur
   $lat = $_GET['lat'];
   $lon = $_GET['lon'];
-} else {
-  // Si les coordonnées ne sont pas fournies, on utilise une position par défaut
-  $lat = 43.296482;
-  $lon = 5.36978;
+
+  // Affichez le marqueur pour la position de l'utilisateur
+  echo $lat . "\t" . $lon . "\t" . "Ma position" . "\t" . "Je suis ici" . "\t" . "pngegg.png" . "\t24,24\t0,-24\n";
+
+  // Connexion à la base de données et récupération des POI à proximité (comme avant)
+  $base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
+  $base->exec("SET CHARACTER SET utf8");
+  $retour = $base->query('SELECT *, get_distance_metres(\''.$lat.'\', \''.$lon.'\', equi_lat, equi_long) AS proximite FROM equipement HAVING proximite < 1000 ORDER BY proximite ASC LIMIT 10;');
+
+  // Boucle pour afficher les POI
+  while ($data = $retour->fetch()){
+    echo $data['equi_lat'] . "\t" . $data['equi_long'] . "\t" . $data['equi_nom'] . "\t" . $data['equi_ad1'] . "\tOl_icon_red_example.png\t24,24\t0,-24\n";
+  }
 }
-
-// Préparation de requête et exécution
-$retour = $base->query('SELECT *, get_distance_metres(:lat, :lon, equi_lat, equi_long) 
-AS proximite 
-FROM equipement 
-HAVING proximite < 1000 ORDER BY proximite ASC
-LIMIT 10;
-');
-$retour->bindParam(':lat', $lat);
-$retour->bindParam(':lon', $lon);
-$retour->execute();
-
-// Affichage des POIs
-echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
-while ($data = $retour->fetch()){
-  echo $data['equi_lat']."\t".$data['equi_long']."\t".$data['equi_nom']."\t".$data['equi_ad1']."\tOl_icon_red_example.png\t24,24\t0,-24\n";
-}
-
 ?>
+
