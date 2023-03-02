63a0c5bac752926768a9e6cd99309aa6be9e6537
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-02 09:54:36 +0100

    push

--- POI.php
+++ POI.php
@@ -3,7 +3,7 @@
 //C'est le POI de l'utilisateur
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
 echo "48.858205\t2.294359\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
-echo "48.842053\t2.355763\tMusée d'Histoire Naturelle de Paris\tAdresse du Musée\tOl_icon_yellow_example.png\t24,24\t0,-24\n";
+echo "48.842053\t2.355763\tMusée d'Histoire Naturelle de Paris\tAdresse du Musée\tOl_icon_red_example.png\t24,24\t0,-24\n";
 
 
 //1° - Connexion à la BDD
