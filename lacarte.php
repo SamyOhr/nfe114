63fbf04ed2a262380818c4a132be6139c0df6196
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-02 10:52:16 +0100

    a

--- lacarte.php
+++ lacarte.php
@@ -1,45 +1,60 @@
+<?php
+//Récupération des coordonnées GPS choisies par l'utilisateur
+if (isset($_POST['lat']) && isset($_POST['lon'])) {
+  $lat = $_POST['lat'];
+  $lon = $_POST['lon'];
+} else {
+  //Si les coordonnées n'ont pas encore été entrées, on initialise à un emplacement par défaut
+  $lat = 43.296482;
+  $lon = 5.36978;
+}
+?>
+
 <html>
   <head>
     <script src="https://openlayers.org/api/OpenLayers.js"></script>
   </head>
   <body>
     <div id="mapdiv"></div>
-    <form method="post" action="lacarte.php">
-      Latitude : <input type="text" name="lat" id="lat">
-      Longitude: <input type="text" name="lon" id="lon">
+    <form method="post">
+      Latitude : <input type="text" name="lat" id="lat" value="<?php echo $lat; ?>">
+      Longitude: <input type="text" name="lon" id="lon" value="<?php echo $lon; ?>">
       <input type="submit" value="Afficher">
     </form>
     <script>
       var map = new OpenLayers.Map("mapdiv");
       map.addLayer(new OpenLayers.Layer.OSM());
 
-      var pois = new OpenLayers.Layer.Text("My Points", {
-        location: "./POI.php",
-        projection: map.displayProjection,
-      });
-      map.addLayer(pois);
+      //Créer un marqueur à l'emplacement choisi
+      var lonLat = new OpenLayers.LonLat(<?php echo $lon; ?>, <?php echo $lat; ?>).transform(
+        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+        map.getProjectionObject() // to Spherical Mercator Projection
+      );
+      var marker = new OpenLayers.Layer.Markers( "Markers" );
+      marker.addMarker(new OpenLayers.Marker(lonLat));
+      map.addLayer(marker);
 
-      <?php
-      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $lat = $_POST['lat'];
-        $lon = $_POST['lon'];
-        echo "var lonLat = new OpenLayers.LonLat($lon, $lat).transform(
-              new OpenLayers.Projection('EPSG:4326'), // transform from WGS 1984
-              map.getProjectionObject() // to Spherical Mercator Projection
-            );
-            var zoom = 12;
-            map.setCenter(lonLat, zoom);";
-      } else {
-        echo "//Set start centrepoint and zoom
-            var lonLat = new OpenLayers.LonLat(5.36978, 43.296482).transform(
-              new OpenLayers.Projection('EPSG:4326'), // transform from WGS 1984
-              map.getProjectionObject() // to Spherical Mercator Projection
-            );
-            var zoom = 12;
-            map.setCenter(lonLat, zoom);";
+      function updateMap() {
+        var lat = document.getElementById("lat").value;
+        var lon = document.getElementById("lon").value;
+        var lonLat = new OpenLayers.LonLat(lon, lat).transform(
+          new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+          map.getProjectionObject() // to Spherical Mercator Projection
+        );
+        var zoom = 12;
+        map.setCenter(lonLat, zoom);
+
+        //Supprimer le marqueur existant
+        map.removeLayer(marker);
+
+        //Créer un nouveau marqueur à l'emplacement choisi
+        marker = new OpenLayers.Layer.Markers( "Markers" );
+        marker.addMarker(new OpenLayers.Marker(lonLat));
+        map.addLayer(marker);
       }
-      ?>
+
+      //Set start centrepoint and zoom
+      map.setCenter(lonLat, 12);
     </script>
   </body>
 </html>
-
